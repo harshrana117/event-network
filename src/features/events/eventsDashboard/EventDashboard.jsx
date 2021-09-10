dbd134c7a95dcf472e2c15d8908133b856a6fadf
Harsh Rana <harshrana117@gmail.com>
2021-09-11 00:14:32 +0530

    Create event, update, add and delete events

--- src/features/events/eventsDashboard/EventDashboard.jsx
+++ src/features/events/eventsDashboard/EventDashboard.jsx
@@ -1,47 +1,55 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Grid } from "semantic-ui-react";
 import EventForm from "../eventForm/EventForm";
 import EventList from "./EventList";
-import {sampleData} from "../../../app/api/sampleData";
+import { sampleData } from "../../../app/api/sampleData";
 
-export default function EventDashboard({formOpen, setFormOpen, selectEvent, selectedEvent, upda}) {
+export default function EventDashboard({
+  formOpen,
+  setFormOpen,
+  selectEvent,
+  selectedEvent,
+  upda,
+}) {
   const [events, setEvents] = useState(sampleData);
-  
 
   function handleCreateEvent(event) {
-    setEvents([...events, event])
+    setEvents([...events, event]);
   }
 
   function handleUpdateEvent(updatedEvent) {
-    setEvents(events.map(evt => evt.id === updatedEvent.id ? updatedEvent : evt));
+    setEvents(
+      events.map((evt) => (evt.id === updatedEvent.id ? updatedEvent : evt))
+    );
     selectEvent(null);
   }
 
   function handleDeleteEvent(eventId) {
-    setEvents(events.filter(evt => evt.id !== eventId));
+    setEvents(events.filter((evt) => evt.id !== eventId));
   }
 
-  
   return (
     <Grid>
-      
       <Grid.Column width={10}>
-        <EventList events={events} selectEvent={selectEvent} deleteEvent={handleDeleteEvent} />
+        <EventList
+          events={events}
+          selectEvent={selectEvent}
+          deleteEvent={handleDeleteEvent}
+        />
       </Grid.Column>
 
       <Grid.Column width={6}>
-        {formOpen && 
-        (
-        <EventForm 
-          setFormOpen={setFormOpen} 
-          setEvents={setEvents} 
-          createEvent={handleCreateEvent}
-          selectedEvent={selectedEvent}
-          updateEvent={handleUpdateEvent}
-          key={selectedEvent ? selectedEvent.id : null}
-        />
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEvents={setEvents}
+            createEvent={handleCreateEvent}
+            selectedEvent={selectedEvent}
+            updateEvent={handleUpdateEvent}
+            key={selectedEvent ? selectedEvent.id : null}
+          />
         )}
-      </Grid.Column>    
+      </Grid.Column>
     </Grid>
-  )
+  );
 }
