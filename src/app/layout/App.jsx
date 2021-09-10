e0c1982e330c76f4071abcca309461a0bdb60a13
Harsh Rana <harshrana117@gmail.com>
2021-09-10 11:56:26 +0530

    Initial setup plus few files added

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/eventsDashboard/EventDashboard";
+import Navbar from "../../navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <h1>Re-vents</h1>
+      <Navbar />
+      <Container className='main'>
+        <EventDashboard />
+      </Container> 
+    </>
+  );
+}
+
+export default App;
